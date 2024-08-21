c5dae990b3220676bb416ef78f47906a79e27403
lempac <brunis.biksis@gmail.com>
2024-08-21 22:37:57 +0300

    Update

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShell.${system} = pkgs.mkShell{
+        packages = [
+            (pkgs.python3.withPackages (py: [
+              py.discordpy
+              py.python-dotenv
+            ]))
+            pkgs.pyright
+        ];
+    };
+  };
+}
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from enum import Enum
 import os
-from typing import Optional
+from typing import *
 
-import discord
-from discord import app_commands
-from dotenv import load_dotenv
+from discord import *
+from dotenv import load_dotenv, set_key
 
+# loads .env file
 load_dotenv()
 
-MY_GUILD = discord.Object(id=os.getenv('guild_id'))  # replace with your guild id
+MY_GUILD = Object(id=os.getenv("guild_id"))  # edit .env to replace with your guild id
 
 
-class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
+class MyClient(Client):
+    def __init__(self, *, intents: Intents):
         super().__init__(intents=intents)
         # A CommandTree is a special type that holds all the application command
         # state required to make it work. This is a separate class because it
@@ -31,55 +32,74 @@ class MyClient(discord.Client):
         await self.tree.sync(guild=MY_GUILD)
 
 
-intents = discord.Intents.default()
-client = MyClient(intents=intents)
+client = MyClient(intents=Intents.default())
 
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
+    print(f"Logged in as {client.user} (ID: {client.user.id})")
+    print("------")
 
 
-@client.tree.command()
-async def hello(interaction: discord.Interaction):
-    """Says hello!"""
-    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+@client.event
+async def on_voice_state_update(member: Member, before: VoiceState, after: VoiceState):
+    if after.channel is None or after.channel.id != int(os.getenv("lobby") or 0):
+        return
+    players = after.channel.members
+    if len(players) < int(os.getenv("max_players") or 10):
+        return
 
 
-@client.tree.command()
+@client.tree.command(description="Voids your current game...")
+@app_commands.guild_only
+async def void(interaction: Interaction):
+    await interaction.response.send_message("test")
+
+
+@client.tree.command(description="Add or remove elo...")
 @app_commands.describe(
-    first_value='The first value you want to add something to',
-    second_value='The value you want to add to the first value',
+    option="Option want to do...",
+    amount="Amount of elo...",
+    member="Member to change...",
 )
-async def add(interaction: discord.Interaction, first_value: int, second_value: int):
-    """Adds two numbers together."""
-    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
+@app_commands.guild_only
+@app_commands.default_permissions(**dict(Permissions.elevated()))
+async def elo(
+    interaction: Interaction,
+    option: Literal["give", "remove"],
+    amount: int,
+    member: Member,
+):
+    pass
 
 
-# The rename decorator allows us to change the display of the parameter on Discord.
-# In this example, even though we use `text_to_send` in the code, the client will use `text` instead.
-# Note that other decorators will still refer to it as `text_to_send` in the code.
-@client.tree.command()
-@app_commands.rename(text_to_send='text')
-@app_commands.describe(text_to_send='Text to send in the current channel')
-async def send(interaction: discord.Interaction, text_to_send: str):
-    """Sends the text into the current channel."""
-    await interaction.response.send_message(text_to_send)
+@client.tree.command(description="Setup up server for usage...")
+@app_commands.default_permissions(**dict(Permissions.elevated()))
+@app_commands.guild_only
+async def setup(interaction: Interaction):
+    if os.getenv("lobby") is None:
+        pass
+
+
+@client.tree.command(description="Registers user for games...")
+@app_commands.describe(name="Username...")
+@app_commands.guild_only
+async def register(interaction: Interaction, name: str):
+    pass
 
 
 # To make an argument optional, you can either give it a supported default argument
 # or you can mark it as Optional from the typing standard library. This example does both.
-@client.tree.command()
-@app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the '
-                              'command')
-async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
-    """Says when a member joined."""
-    # If no member is explicitly provided then we use the command user here
-    member = member or interaction.user
-
-    # The format_dt function formats the date time into a human-readable representation in the official client
-    await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+# @client.tree.command()
+# @app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the '
+#                               'command')
+# async def joined(interaction: Interaction, member: Optional[Member] = None):
+#     """Says when a member joined."""
+#     # If no member is explicitly provided then we use the command user here
+#     member = member or interaction.user
+#
+#     # The format_dt function formats the date time into a human-readable representation in the official client
+#     await interaction.response.send_message(f'{member} joined {utils.format_dt(member.joined_at)}')
 
 
 # A Context Menu command is an app command that can be run on a member or on a message by
@@ -87,34 +107,34 @@ async def joined(interaction: discord.Interaction, member: Optional[discord.Memb
 # It always takes an interaction as its first parameter and a Member or Message as its second parameter.
 
 # This context menu command only works on members
-@client.tree.context_menu(name='Show Join Date')
-async def show_join_date(interaction: discord.Interaction, member: discord.Member):
-    # The format_dt function formats the date time into a human-readable representation in the official client
-    await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
+# @client.tree.context_menu(name='Show Join Date')
+# async def show_join_date(interaction: discord.Interaction, member: discord.Member):
+#     # The format_dt function formats the date time into a human-readable representation in the official client
+#     await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
 
 
 # This context menu command only works on messages
-@client.tree.context_menu(name='Report to Moderators')
-async def report_message(interaction: discord.Interaction, message: discord.Message):
-    # We're sending this response message with ephemeral=True, so only the command executor can see it
-    await interaction.response.send_message(
-        f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
-    )
+# @client.tree.context_menu(name='Report to Moderators')
+# async def report_message(interaction: discord.Interaction, message: discord.Message):
+#     # We're sending this response message with ephemeral=True, so only the command executor can see it
+#     await interaction.response.send_message(
+#         f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
+#     )
+#
+#     # Handle report by sending it into a log channel
+#     log_channel = interaction.guild.get_channel(0)  # replace with your channel id
+#
+#     embed = discord.Embed(title='Reported Message')
+#     if message.content:
+#         embed.description = message.content
+#
+#     embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+#     embed.timestamp = message.created_at
+#
+#     url_view = discord.ui.View()
+#     url_view.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
+#
+#     await log_channel.send(embed=embed, view=url_view)
 
-    # Handle report by sending it into a log channel
-    log_channel = interaction.guild.get_channel(0)  # replace with your channel id
 
-    embed = discord.Embed(title='Reported Message')
-    if message.content:
-        embed.description = message.content
-
-    embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
-    embed.timestamp = message.created_at
-
-    url_view = discord.ui.View()
-    url_view.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
-
-    await log_channel.send(embed=embed, view=url_view)
-
-
-client.run(os.getenv('token'))
+client.run(os.getenv("token"))
